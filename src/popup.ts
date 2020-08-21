f5deddfe2989a9c745990c62d1fecf51cb13bb4a
t1732 <oo1732oo@gmail.com>
2020-08-22 01:09:35 +0900

    [chore] stylus 導入

--- src/popup.ts
+++ src/popup.ts
@@ -1,3 +1,5 @@
+import "./popup.styl"
+
 const popup = () => {
 
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -2,13 +2,14 @@ import { ConfigurationFactory } from "webpack"
 import path from "path"
 import CopyPlugin from "copy-webpack-plugin"
 import HtmlWebpackPlugin from "html-webpack-plugin"
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
 
 const config: ConfigurationFactory = () => {
   return {
     mode: "production",
     entry: {
       content_scripts: path.join(__dirname, "src", "content_scripts.ts"),
-      menu: path.join(__dirname, "src","popup.ts"),
+      popup: path.join(__dirname, "src","popup.ts")
     },
     output: {
       path: path.join(__dirname, "dist"),
@@ -24,21 +25,38 @@ const config: ConfigurationFactory = () => {
         {
           test: /\.pug$/,
           use: "pug-loader"
+        },
+        {
+          test: /\.styl$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "stylus-loader"
+          ]
         }
       ]
     },
     resolve: {
-      extensions: [".ts", ".js"]
+      extensions: [".ts", ".js", ".styl"]
     },
     plugins: [
       new CopyPlugin({
         patterns: [
-          { from: "public", to: "." }
+          {
+            from: "public",
+            to: ".",
+            globOptions: {
+              dot: false
+            }
+          }
         ],
       }),
       new HtmlWebpackPlugin({
         filename: "popup.html",
         template: "./src/popup.pug"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css"
       })
     ]
   }
